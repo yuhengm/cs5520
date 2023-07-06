9df272bd8f97c4df90e31fa607b60167c21d2f8c
Yuheng <miao.yuhe@northeastern.edu>
2023-07-06 16:26:38 -0700

    add text input

--- session2/AwesomeProject/App.js
+++ session2/AwesomeProject/App.js
@@ -1,14 +1,18 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, TextInput, View } from 'react-native';
 import Header from './components/Header';
+import { useState } from 'react';
 
 export default function App() {
+
   const appName = "CS 5220"
+  const [text, setText] = useState('');
+
   return (
     <View style={styles.container}>
       <Header name={appName}></Header>
-      <Text>Child 1</Text>
-      <Text>Child 2</Text>
+      <Text>Text Child</Text>
+      <TextInput style={styles.input} placeholder="Type something here" onChangeText={newText => setText(newText)} defaultValue={text} />
       <StatusBar style="auto" />
     </View>
   );
@@ -21,4 +25,8 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  input: {
+    borderBottomWidth: 1,
+    width: 200
+  }
 });
