e43d07c62f3778d73a547e553c4380805ddaf3e4
Yuheng <miao.yuhe@northeastern.edu>
2023-07-10 15:20:08 -0700

    change name and edit input

--- class-activity/AwesomeProject/components/Input.js
+++ class-activity/AwesomeProject/components/Input.js
@@ -1,11 +1,16 @@
-import { View, Text, TextInput, StyleSheet } from 'react-native';
+import { View, Text, TextInput, StyleSheet, Button } from 'react-native';
 import { React, useState } from 'react';
 
 const Input = ({ handleTextChange }) => {
 
+    const checkButton = () => {
+        console.log("button pressed");
+    }
+
     return (
         <View>
             <TextInput style={styles.input} placeholder="Type something here" onChangeText={handleTextChange} />
+            <Button title="Confirm" onPress={checkButton} />
         </View>
     );
 }
