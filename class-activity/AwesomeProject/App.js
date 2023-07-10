2bc57ae70bd33493a7f178661af146a3f9045db5
Yuheng <miao.yuhe@northeastern.edu>
2023-07-10 16:33:58 -0700

    add stylings

--- class-activity/AwesomeProject/App.js
+++ class-activity/AwesomeProject/App.js
@@ -24,7 +24,7 @@ export default function App() {
       <Header name={appName}></Header>
       <Input text={text} handleTextChange={handleTextChange} modalVisibility={modalVisibility} setVisible={setVisible} setInvisible={setInvisible} />
       {/* Re-render when text changes */}
-      <Text>Goal: {text}</Text>
+      <Text style={styles.text}>Goal: {text}</Text>
       <Button title="Add A Goal" onPress={setVisible} />
       <StatusBar style="auto" />
     </View>
@@ -38,8 +38,7 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
-  input: {
-    borderBottomWidth: 1,
-    width: 200
+  text: {
+    color: "#a09"
   }
 });
--- class-activity/AwesomeProject/components/Header.js
+++ class-activity/AwesomeProject/components/Header.js
@@ -1,12 +1,27 @@
-import { View, Text } from 'react-native'
+import { StyleSheet, View, Text } from 'react-native'
 import React from 'react'
 
 const Header = ({ name }) => {
     return (
-        <View>
-            <Text>Welcome to {name}</Text>
+        <View style={styles.header}>
+            <Text style={styles.text}>Welcome to {name}</Text>
         </View>
     )
 }
+
+const styles = StyleSheet.create({
+    header: {
+        borderColor: "darkslateblue",
+        borderWidth: 3,
+        borderRadius: 10,
+        padding: 10,
+        margin: 10
+    },
+    text: {
+        fontSize: 20,
+        fontWeight: "bold",
+        color: "darkslateblue"
+    }
+});
 
 export default Header
