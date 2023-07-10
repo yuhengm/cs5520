bf45642e14ffa581265f9864ebe8255ec2f4ffdf
Yuheng <miao.yuhe@northeastern.edu>
2023-07-10 16:08:26 -0700

    add modal

--- class-activity/AwesomeProject/App.js
+++ class-activity/AwesomeProject/App.js
@@ -1,13 +1,20 @@
 import { React, useState } from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, View, Text } from 'react-native';
+import { StyleSheet, View, Text, Button } from 'react-native';
 import Header from './components/Header';
 import Input from './components/Input';
 
 export default function App() {
 
   const appName = "CS 5220"
-  const [text, setText] = useState(""); // async
+  const [text, setText] = useState("") // async
+  const [modalVisibility, setModalVisibility] = useState(false)
+  const setVisible = () => {
+    setModalVisibility(true);
+  }
+  const setInvisible = () => {
+    setModalVisibility(false);
+  }
   const handleTextChange = (newText) => {
     setText(newText)
   }
@@ -15,9 +22,10 @@ export default function App() {
   return (
     <View style={styles.container}>
       <Header name={appName}></Header>
-      <Input text={text} handleTextChange={handleTextChange} />
+      <Input text={text} handleTextChange={handleTextChange} modalVisibility={modalVisibility} setVisible={setVisible} setInvisible={setInvisible} />
       {/* Re-render when text changes */}
       <Text>Received: {text}</Text>
+      <Button title="Add A Goal" onPress={setVisible} />
       <StatusBar style="auto" />
     </View>
   );
--- class-activity/AwesomeProject/components/Input.js
+++ class-activity/AwesomeProject/components/Input.js
@@ -1,24 +1,33 @@
-import { View, TextInput, StyleSheet, Button } from 'react-native';
+import { View, TextInput, StyleSheet, Button, Modal } from 'react-native';
 import { React, useState } from 'react';
 
-const Input = ({ handleTextChange }) => {
+const Input = ({ handleTextChange, modalVisibility, setVisible, setInvisible }) => {
 
     const [text, setText] = useState("")
     const processStoredText = (text) => {
         handleTextChange(text);
         setText(text);
+        setInvisible();
     }
 
     return (
-        <View>
-            <TextInput style={styles.input} placeholder="Type something here" onChangeText={(text) => setText(text)} value={text} />
-            <Button title="Confirm" onPress={() => processStoredText(text)} />
-            <Button title="Cancel" onPress={() => processStoredText("")} />
-        </View>
+        <Modal visible={modalVisibility}>
+            <View style={styles.container}>
+                <TextInput style={styles.input} placeholder="Type something here" onChangeText={(text) => setText(text)} value={text} />
+                <Button title="Confirm" onPress={() => processStoredText(text)} />
+                <Button title="Cancel" onPress={() => processStoredText("")} />
+            </View>
+        </Modal>
     );
 }
 
 const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
     input: {
         borderBottomWidth: 1,
         width: 200
