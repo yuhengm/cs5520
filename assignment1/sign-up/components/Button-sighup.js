98995a20caafcc7197ff3a4400104a5bb876bc8f
Yuheng <miao.yuhe@northeastern.edu>
2023-07-11 23:12:49 -0700

    add possible screens and components

---
+++ assignment1/sign-up/components/Button-sighup.js
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const Button-sighup = () => {
+    return (
+        <View>
+            <Text>Button-sighup</Text>
+        </View>
+    )
+}
+
+export default Button - sighup
